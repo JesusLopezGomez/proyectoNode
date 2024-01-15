2f7d26c2e18e19c08381524926b6bc6c0241b043
JesusLopezGomez <jelopez.gom@gmail.com>
2024-01-15 15:21:47 +0100

    .

--- controllers/impresora3d.js
+++ controllers/impresora3d.js
@@ -24,10 +24,10 @@ const getImpresora3dById = async(req,res) => {
 }
 
 const addImpresora3d = async(req,res) => {
-    const {marca,modelo,medida,precio} = req.body;
+    const {marca,modelo,medida,email,precio} = req.body;
 
     if(marca && modelo && medida && precio){
-        const newImpresora3d = new Impresora3d({marca,modelo,medida,precio});
+        const newImpresora3d = new Impresora3d({marca,modelo,medida,email,precio});
 
         try{
             await newImpresora3d.save();
