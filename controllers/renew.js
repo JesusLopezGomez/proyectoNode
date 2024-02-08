6ef0e94c3a794e13e442a1b9d976b3a01e94e8b5
JesuLopezGomez <jelopez.gom@gmail.com>
2024-02-08 12:31:33 +0100

    .

--- controllers/renew.js
+++ controllers/renew.js
@@ -1,26 +1,20 @@
 const jwt  = require("jsonwebtoken");
-const usuario = require("../models/usuario");
-const bcryptjs = require("bcryptjs");
 
 const renew = async(req,res) => {
+    const user = req.user;
     try{
-        const usuarioBuscar = await usuario.findOne({email});
-        let validPassword = null;
-        if(usuarioBuscar && usuarioBuscar.active){
-            validPassword = bcryptjs.compareSync(password, usuarioBuscar.password);
-        }
-        if(validPassword){
-            const payload = {uid: usuarioBuscar.id}
+        console.log(user);
+        if(user){
+            const payload = {uid: user.id}
             const token = jwt.sign( payload,process.env.SECRET, {expiresIn: '4h'});
-            res.status(200).json({user: usuarioBuscar,token});
+            res.status(200).json({user: user,token});
         }else{
             res.status(400).json({message:"Email or password invalid..."});
         }
-
-
-
     }catch(err){
         console.log(err);
         res.status(500).json({message:err});
     }
 }
+
+module.exports = {renew}
--- routes/renew.js
+++ routes/renew.js
@@ -4,15 +4,14 @@ const router = express.Router();
 const {check} = require("express-validator");
 
 const { validateFields } = require("../middlewares/validateFields");
-const {login} = require("../controllers/auth");
 const { validateJWT } = require("../middlewares/validateJwt");
-
+const { renew } = require("../controllers/renew");
 
 router.post([
     validateJWT,
     check("username","El nombre de usuario es de tipo Strign y no puede estar vacío").isString(),
     check("password","La contraseña es de tipo String y no puede estar vacía").isString(),
     ,validateFields
-    ],login);
+    ],renew);
 
 module.exports = router;
